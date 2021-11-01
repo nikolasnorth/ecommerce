fd11b7c09388569f23f779bf92d974d0d1d31939
Nikolas North <nikolasn09@gmail.com>
2021-11-01 12:58:46 -0400

    create product service

---
+++ backend/product-service/src/main/java/com/nikolasnorth/productservice/ProductController.java
@@ -0,0 +1,13 @@
+package com.nikolasnorth.productservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+  @GetMapping("/")
+  public String index() {
+    return "Hello world";
+  }
+}
