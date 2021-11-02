97abf52b3f7c22aab6cd02c5951ee9b351eacc36
Nikolas North <nikolasn09@gmail.com>
2021-11-02 11:15:41 -0400

    create account controller

---
+++ backend/account-service/src/main/java/com/nikolasnorth/accountservice/AccountController.java
@@ -0,0 +1,25 @@
+package com.nikolasnorth.accountservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/accounts")
+public class AccountController {
+
+  private final AccountService accountService;
+
+  @Autowired
+  public AccountController(AccountService accountService) {
+    this.accountService = accountService;
+  }
+
+  @GetMapping()
+  public List<Account> index() {
+    return accountService.getAccounts();
+  }
+}
--- backend/account-service/src/main/java/com/nikolasnorth/accountservice/AccountService.java
+++ backend/account-service/src/main/java/com/nikolasnorth/accountservice/AccountService.java
@@ -3,11 +3,18 @@ package com.nikolasnorth.accountservice;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class AccountService {
 
   public Account getAccount(int id) {
     return new Account(1, "nikolas@uwo.ca", "Nikolas", 80, LocalDate.now(), LocalDate.now());
+  }
+
+  public List<Account> getAccounts() {
+    return List.of(
+      new Account(1, "nikolas@uwo.ca", "Nikolas", 80, LocalDate.now(), LocalDate.now())
+    );
   }
 }
