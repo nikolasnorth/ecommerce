fb668c8df41dd151baa4236ca77623f3fde43f58
Nikolas North <nikolasn09@gmail.com>
2021-11-02 11:05:07 -0400

    create account entity

---
+++ backend/account-service/src/main/java/com/nikolasnorth/accountservice/Account.java
@@ -0,0 +1,108 @@
+package com.nikolasnorth.accountservice;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+public class Account {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private int id;
+
+  private String email;
+
+  private String name;
+
+  private int rating;
+
+  @CreatedDate
+  private LocalDate createdAt;
+
+  @LastModifiedDate
+  private LocalDate updatedAt;
+
+  protected Account() {
+  }
+
+  public Account(int id, String email, String name, int rating, LocalDate createdAt, LocalDate updatedAt) {
+    this.id = id;
+    this.email = email;
+    this.name = name;
+    this.rating = rating;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+
+  public Account(String email, String name, int rating) {
+    this.email = email;
+    this.name = name;
+    this.rating = rating;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getRating() {
+    return rating;
+  }
+
+  public void setRating(int rating) {
+    this.rating = rating;
+  }
+
+  public LocalDate getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(LocalDate createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public LocalDate getUpdatedAt() {
+    return updatedAt;
+  }
+
+  public void setUpdatedAt(LocalDate updatedAt) {
+    this.updatedAt = updatedAt;
+  }
+
+  @Override
+  public String toString() {
+    return "Account{" +
+      "id=" + id +
+      ", email='" + email + '\'' +
+      ", name='" + name + '\'' +
+      ", rating=" + rating +
+      ", createdAt=" + createdAt +
+      ", updatedAt=" + updatedAt +
+      '}';
+  }
+}
