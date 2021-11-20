c99888eb5dfc97af7cf67a00488572de79a73dfe
Nikolas North <nikolasn09@gmail.com>
2021-11-20 17:32:40 -0500

    create ProductCard component

---
+++ frontend/components/ProductCard.tsx
@@ -0,0 +1,14 @@
+import {Product} from "@types";
+
+interface ProductCardProps {
+  product: Product;
+}
+
+export default function ProductCard({product}: ProductCardProps) {
+  return (
+    <div key={product?.id} className="bg-gray-700 rounded-lg p-4">
+      <p>{product?.title}</p>
+      <p>{product?.price}</p>
+    </div>
+  );
+}
--- frontend/pages/marketplace.tsx
+++ frontend/pages/marketplace.tsx
@@ -1,5 +1,6 @@
 import {GetServerSidePropsResult} from "next";
 import {Product} from "@types";
+import ProductCard from "@components/ProductCard";
 
 interface MarketplaceProps {
   products: Product[];
@@ -22,14 +23,7 @@ export default function Marketplace({products}: MarketplaceProps) {
         <h3 className="text-3xl font-semibold">Mustang</h3>
       </header>
       <div className="grid gap-4 grid-cols-3 justify-items-stretch min-h-screen">
-        {products?.map(product => {
-          return (
-            <div key={product.id} className="bg-gray-700 rounded-lg p-4">
-              <p>{product.title}</p>
-              <p>{product.price}</p>
-            </div>
-          );
-        })}
+        {products?.map(product => <ProductCard product={product}/>)}
       </div>
     </>
   );
