8d135f079f537d8dd765323cbedc6ff6dfa4ebc9
Nikolas North <nikolasn09@gmail.com>
2021-11-20 16:29:21 -0500

    style index page

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,16 @@
-import {AppProps} from "next/app";
 import 'tailwindcss/tailwind.css';
+import {AppProps} from "next/app";
+import Head from "next/head";
 
 export default function MyApp({Component, pageProps}: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Mustang</title>
+      </Head>
+      <div className="bg-gray-900 h-screen text-white">
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <>
-      <h1>Hello, world.</h1>
-    </>
+    <main className="flex flex-col justify-center items-center h-screen">
+      <h1 className="text-7xl font-medium mb-12">Welcome to <b>Mustang</b></h1>
+      <Link href={"marketplace"}>
+        <a className="bg-blue-600 px-8 py-2 rounded-md">Get started</a>
+      </Link>
+    </main>
   );
 }
