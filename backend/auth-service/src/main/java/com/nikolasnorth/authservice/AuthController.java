c66ba48ea8e7da3f20db9259bdd87e6b7049e74d
Nikolas North <nikolasn09@gmail.com>
2021-11-23 10:09:44 -0500

    fix imports

--- backend/auth-service/src/main/java/com/nikolasnorth/authservice/AuthController.java
+++ backend/auth-service/src/main/java/com/nikolasnorth/authservice/AuthController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
