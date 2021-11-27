c037760bc0b8e636914c67631c2c19e7413bdb72
Nikolas North <nikolasn09@gmail.com>
2021-11-27 11:17:56 -0500

    publish events to amazon sns

---
+++ backend/account-service/src/main/java/com/nikolasnorth/accountservice/sns/SnsService.java
@@ -0,0 +1,28 @@
+package com.nikolasnorth.accountservice.sns;
+
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.model.PublishResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SnsService {
+
+  @Value("${sns.topic.arn}")
+  private String snsTopicArn;
+
+  private final AmazonSNS amazonSns;
+
+  @Autowired
+  public SnsService(AmazonSNS a) {
+    this.amazonSns = a;
+  }
+
+  @Scheduled(fixedDelay = 15_000)  // 15 seconds
+  public void publish() {
+    PublishResult res = amazonSns.publish(snsTopicArn, "account-service");
+    System.out.printf("Message ID: %s%n", res.getMessageId());
+  }
+}
