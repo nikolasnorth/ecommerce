a156f0cf5378118dd1735aa4abadde0c3cda696e
Nikolas North <nikolasn09@gmail.com>
2021-12-01 18:24:38 -0500

    contact service registry prior to forwarding request

--- backend/api-gateway/src/main/java/com/nikolasnorth/apigateway/ApiGatewayRouteLocator.java
+++ backend/api-gateway/src/main/java/com/nikolasnorth/apigateway/ApiGatewayRouteLocator.java
@@ -1,36 +1,99 @@
 package com.nikolasnorth.apigateway;
 
 import com.nikolasnorth.apigateway.filters.JwtFilterFactory;
+import com.nikolasnorth.apigateway.filters.RouteLookupFilterFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Optional;
 
 @Service
 public class ApiGatewayRouteLocator {
 
-  private final ApiGatewayCache cache;
+  private final RestTemplate restTemplate;
+
+  private final String serviceRegistryUrl;
 
   @Autowired
-  public ApiGatewayRouteLocator(ApiGatewayCache cache) {
-    this.cache = cache;
+  public ApiGatewayRouteLocator(RestTemplate restTemplate, @Value("${serviceRegistry.url}") String serviceRegistryUrl) {
+    this.restTemplate = restTemplate;
+    this.serviceRegistryUrl = serviceRegistryUrl;
   }
 
   @Bean
-  public RouteLocator gateway(RouteLocatorBuilder builder, JwtFilterFactory jwtFilterFactory) {
+  public RouteLocator gateway(
+    RouteLocatorBuilder builder,
+    JwtFilterFactory jwtFilterFactory,
+    RouteLookupFilterFactory routeLookupFilterFactory
+  ) {
     return builder.routes()
       .route(ECommerceService.ACCOUNT.label, p -> p
         .path("/api/v1/accounts")
         .or()
         .path("/api/v1/accounts/*")
-        .filters(f -> f.filter(jwtFilterFactory.apply(new JwtFilterFactory.Config())))
-        .uri("http://localhost:8082/"))
+        .filters(f -> f
+          .filter(jwtFilterFactory.apply(new JwtFilterFactory.Config()))
+          .changeRequestUri((serverWebExchange -> {
+            try {
+              final var res
+                = restTemplate.getForEntity(String.format("%s/%d", serviceRegistryUrl, ECommerceService.ACCOUNT.id), ServiceRegistryResponseBody.class);
+              if (res.getStatusCode() != HttpStatus.OK) {
+                System.err.printf("Did not receive 200 status code for %s response%n", ECommerceService.ACCOUNT.label);
+                serverWebExchange.getResponse().setStatusCode(HttpStatus.SERVICE_UNAVAILABLE);
+                return Optional.empty();
+              }
+
+              ServiceRegistryResponseBody body = res.getBody();
+              if (body == null || body.getServiceLocation() == null) {
+                System.err.printf("Did not receive 200 status code for %s response%n", ECommerceService.ACCOUNT.label);
+                serverWebExchange.getResponse().setStatusCode(HttpStatus.SERVICE_UNAVAILABLE);
+                return Optional.empty();
+              }
+
+              return Optional.of(new URI(body.getServiceLocation() + serverWebExchange.getRequest().getPath()));
+            } catch (URISyntaxException e) {
+              System.err.println("Error occurred making URI");
+              return Optional.empty();
+            }
+          })))
+        .uri("no://op"))
       .route(ECommerceService.AUTH.label, p -> p
         .path("/api/v1/auth")
         .or()
         .path("/api/v1/auth/*")
-        .uri("http://localhost:8085/"))
+        .filters(f -> f
+          .changeRequestUri((serverWebExchange -> {
+            try {
+              final var res
+                = restTemplate.getForEntity(String.format("%s/%d", serviceRegistryUrl, ECommerceService.AUTH.id), ServiceRegistryResponseBody.class);
+              if (res.getStatusCode() != HttpStatus.OK) {
+                System.err.printf("Did not receive 200 status code for %s response%n", ECommerceService.AUTH.label);
+                serverWebExchange.getResponse().setStatusCode(HttpStatus.SERVICE_UNAVAILABLE);
+                return Optional.empty();
+              }
+
+              ServiceRegistryResponseBody body = res.getBody();
+              if (body == null || body.getServiceLocation() == null) {
+                System.err.printf("Did not receive 200 status code for %s response%n", ECommerceService.AUTH.label);
+                serverWebExchange.getResponse().setStatusCode(HttpStatus.SERVICE_UNAVAILABLE);
+                return Optional.empty();
+              }
+
+              return Optional.of(new URI(body.getServiceLocation() + serverWebExchange.getRequest().getPath()));
+            } catch (URISyntaxException e) {
+              System.err.println("Error occurred making URI");
+              return Optional.empty();
+            }
+          })))
+        .uri("no://op"))
       .build();
   }
 }
